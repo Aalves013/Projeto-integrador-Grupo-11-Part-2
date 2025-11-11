12afde7cf47f2b35847000856965b1e20f0bab6f
Henrique Alves <henriqueaalves.souza@gmail.com>
2025-11-11 19:59:08 -0300

    Att 3000 to 3011

--- api/src/server.ts
+++ api/src/server.ts
@@ -22,6 +22,6 @@ UserController();
 MedicamentoController();
 SolicitacaoController();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(3011, () => {
+  console.log("Server is running on port 3011");
 });
